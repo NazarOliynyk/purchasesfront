1dc7329f81bde15d163f35ba9c6a1fcb016fdaac
Your Name <nazar_lw@ukr.net>
2019-08-19 17:59:54 +0300

    URL changed.

--- src/app/services/main-service.service.ts
+++ src/app/services/main-service.service.ts
@@ -10,8 +10,8 @@ import {Purchase} from '../models/Purshase';
 })
 export class MainServiceService {
 
-  // url = 'http://localhost:8080';
-  url = 'http://ec2-18-222-5-217.us-east-2.compute.amazonaws.com:8080';
+  url = 'http://localhost:8080';
+  // url = 'http://ec2-18-222-5-217.us-east-2.compute.amazonaws.com:8080';
 
   constructor(
     private http: HttpClient
